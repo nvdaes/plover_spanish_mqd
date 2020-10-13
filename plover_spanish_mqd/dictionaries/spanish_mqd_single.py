07cd61ecd571a255229e773211fb4558481826b2
Noelia Ruiz Martínez <nrm1977@gmail.com>
2020-10-13 23:02:00 +0200

    Todo: see final lines of code

--- plover_spanish_mqd/dictionaries/spanish_mqd_single.py
+++ plover_spanish_mqd/dictionaries/spanish_mqd_single.py
@@ -1,40 +1,59 @@
 LONGEST_KEY = 1
 
 dict = {
-	"SC": ("casa", "ca", False),
+	"SC": ("casa ", "ca", True),
 	"N": ("nana", "n", False),
 	"Ccn": ("camina{p}", "camin", False)
 }
 
 lastValue = ""
+shouldWait = False
+
+def isInitial(value):
+	if value == "" or value.endswith("{i}") or value[0] == " ":
+		return True
+	return False
+
+def isWaiting():
+	if lastValue != "" and lastValue.endswith("{p}"):
+		return True
+	return False
+
+def searchKey(dictionary, stroke):
+	searchKey = stroke[:]
+	searchKeyValue = ""
+	lenSearched = 0
+	value = ""
+	while len(searchKey) > 0:
+		if dictionary.get(searchKey) is not None:
+			searchKeyValue = dictionary.get(searchKey)[1]
+		if searchKeyValue == "":
+			searchKey = searchKey[:len(searchKey)-1]
+		else:
+			value += searchKeyValue
+			searchKeyValue = ""
+			lenSearched += len(searchKey)
+			searchKey = stroke[lenSearched:]
+	return value
 
 def lookup(key):
-	global lastValue
+	global lastValue, shouldWait
 	value = ""
-	if "{p}" in lastValue:
-        value = lastValue.replace(""{p", "")
 	if dict.get(key[0]) is not None:
-
+		if lastValue == "" or lastValue.endswith("{i}") or lastValue[-1] == " " or not dict.get(key[0])[2]:
 			value = dict.get(key[0])[0] 
+			if value.endswith("{p}"):
+				lastValue = value[:-3]
+				shouldWait = True
+				return " "
 		if value == "":
-			value = dict.get(key[0])[1]
+			value = dict.get(key[0])[1] 
 	if value == "":
-		searchKey = key[0][:]
-		searchKeyValue = ""
-		lenSearched = 0
-		while len(searchKey) > 0:
-			if dict.get(searchKey) is not None:
-				searchKeyValue = dict.get(searchKey)[1]
-			if searchKeyValue == "":
-				searchKey = searchKey[:len(searchKey)-1]
-			else:
-				value += searchKeyValue
-				searchKeyValue = ""
-				lenSearched += len(searchKey)
-				searchKey = key[0][lenSearched:]
-	if "{p}" in value:
-		value.replace("{p}", "")
-		lastValue = value
-		return " "
+		value = searchKey(dict, key[0])
+	if shouldWait:
+		lastValue = lastValue + value
+	value.replace("{i}", "")
 	lastValue = value
-	return value
+	if value.endswith(" "):
+		return value
+	return value + "{^}"
