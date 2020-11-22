2abc78acd38a4c6ee3adf0cec7837976396a6aff
Noelia Ruiz Martínez <nrm1977@gmail.com>
2020-11-22 06:36:30 +0100

    Fix tests

--- tests/test_single.py
+++ tests/test_single.py
@@ -1,4 +1,11 @@
 import unittest
+import os
+import sys
+UNIT_DIR = os.path.dirname(os.path.abspath(__file__))
+TOP_DIR = os.path.dirname(os.path.dirname(UNIT_DIR))
+SOURCE_DIR = os.path.join(TOP_DIR, "plover_spanish_mqd")
+DICT_DIR = os.path.join(TOP_DIR, "dictionaries")
+sys.path.append(DICT_DIR)
 
 import spanish_mqd_single
 
