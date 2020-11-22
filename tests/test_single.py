8eb4b43193c0158ccd5ff79fc810cb3e42d7075d
Noelia Ruiz Martínez <nrm1977@gmail.com>
2020-11-22 08:02:02 +0100

    Check test

--- tests/test_single.py
+++ tests/test_single.py
@@ -1,20 +1,20 @@
 import unittest
 from plover_spanish_mqd.dictionaries import spanish_mqd_single
 
-def isDictWellFormed():
-	dict = spanish_mqd_single.dict
-	for k, v in dict.items():
-		if not isinstance(k, str):
-			raise TypeError("Invalid key")
-		if len(v) != 3:
-			raise ValueError("Tuple with invalid length")
-		for fragment in v:
-			if not isinstance(fragment, str):
-				raise TypeError("Invalid value")
-	return True
-
 class TestSingle(unittest.TestCase):
 
-	def test_single(self):
-		self.assertTrue(isDictWellFormed)
+	def setUp(self):
+		self.dict = spanish_mqd_single.dict
+
+	def test_keyType(self):
+		for k, v in self.dict.items():
+			self.assertTrue(isinstance(k, str))
+
+	def test_valueType(self):
+		for k, v in self.dict.items():
+			self.assertTrue(isinstance(v, tuple))
+
+	def test_valueLenght(self):
+		for k, v in self.dict.items():
+			self.assertTrue(len(v) == 3)
 
