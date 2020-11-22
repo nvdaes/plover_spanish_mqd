64e87207baef44e50cf385389db042258262d796
Noelia Ruiz Martínez <nrm1977@gmail.com>
2020-11-22 12:25:28 +0100

    Add test for initial

---
+++ tests/test_initial.py
@@ -0,0 +1,42 @@
+import unittest
+import os
+import json
+from plover_spanish_mqd import system
+
+DICT = os.path.join(DICT_DIR, "initial.json")
+
+
+class TestSingle(unittest.TestCase):
+
+	def setUp(self):
+		with open(DICT) as f:
+			self.dict = json.load(f)
+		self.keys = "".join(system.KEYS)
+
+	def tearDown(self):
+		self.dict = None
+		self.keys = None
+
+	def test_keyType(self):
+		for k, v in self.dict.items():
+			self.assertTrue(isinstance(k, str), k)
+
+	def test_keyOrder(self):
+		for k, v in self.dict.items():
+			prevIndex = -1
+			curIndex = -1
+			for char in k:
+				curIndex = self.keys.find(char)
+				self.assertTrue(curIndex > prevIndex, k)
+				prevIndex = curIndex
+
+	def test_valueType(self):
+		for k, v in self.dict.items():
+			self.assertTrue(isinstance(v, str), k)
+
+	def test_command(self)):
+		for k, v in self.dict.items():
+			if v.startswith(":initial:"):
+				self.assertEqual(len(v.split(" | ")), 2, k)
+
+
