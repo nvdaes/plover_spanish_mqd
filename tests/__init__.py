335d44a513914ddf5fd121360eb055b8576c3cd6
Noelia Ruiz Martínez <nrm1977@gmail.com>
2022-05-02 08:06:41 +0200

    Detect duplicate keys in json files

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,21 @@
 import os
 import sys
+
 UNIT_DIR = os.path.dirname(os.path.abspath(__file__))
 TOP_DIR = os.path.dirname(os.path.dirname(UNIT_DIR))
 SOURCE_DIR = os.path.join(TOP_DIR, "plover_spanish_mqd")
 DICT_DIR = os.path.join(SOURCE_DIR, "dictionaries")
 sys.path.append(DICT_DIR)
+
+
+def detectDuplicateKeys(orderedPairs):
+	d = {}
+	duplicateKeys = []
+	for k, v in orderedPairs:
+		if k in d:
+			duplicateKeys.append(k)
+		else:
+			d[k] = v
+	if len(duplicateKeys):
+		raise ValueError("\n".join(duplicateKeys))
+	return d
--- tests/test_punctuation.py
+++ tests/test_punctuation.py
@@ -1,7 +1,9 @@
 import unittest
 import os
 import json
+
 from plover_spanish_mqd import system
+from . import detectDuplicateKeys
 
 DICT = os.path.join(os.path.dirname(__file__), "..", "plover_spanish_mqd", "dictionaries", "punctuation.json")
 
@@ -11,7 +13,7 @@ class TestPunctuation(unittest.TestCase):
 	def setUp(self):
 		self.keys = "".join(system.KEYS)
 		with open(DICT, encoding='utf-8') as f:
-			self.dict = json.load(f)
+			self.dict = json.load(f, object_pairs_hook=detectDuplicateKeys)
 
 	def tearDown(self):
 		self.keys = None
