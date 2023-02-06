beb87d82e8bbf40d176501baddb0b60174dee94c
Noelia Ruiz Martínez <nrm1977@gmail.com>
2023-02-06 12:14:29 +0100

    Try to improve tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -3,6 +3,7 @@ import os
 import json
 
 from plover_spanish_mqd import system
+from plover_spanish_mqd import spanish_mqd_single
 from . import detectDuplicateKeys
 
 DICT = os.path.join(os.path.dirname(__file__), "..", "plover_spanish_mqd", "dictionaries", "user_es.json")
@@ -12,11 +13,13 @@ class TestUser(unittest.TestCase):
 
 	def setUp(self):
 		self.keys = "".join(system.KEYS)
+		self.singleDict = spanish_mqd_single.dict
 		with open(DICT, encoding="utf-8") as f:
 			self.dict = json.load(f, object_pairs_hook=detectDuplicateKeys)
 
 	def tearDown(self):
 		self.keys = None
+		self.singleDict = None
 		self.dict = None
 
 	def test_keyOrder(self):
@@ -39,3 +42,9 @@ class TestUser(unittest.TestCase):
 	def test_ValueContent(self):
 		for k, v in self.dict.items():
 			self.assertFalse("//" in v, k)
+
+	def test_duplicateKeysInSingle(self):
+		dictKeys = self.dict.keys()
+		singleDictKeys = self.singleDict.keys()
+		dictKeys.extend(singleDictKeys)
+		self.assertEqual(len(dictKeys), len(set(dictKeys)))
